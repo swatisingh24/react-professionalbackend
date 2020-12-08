e00e70b910685ff105e59263712b6084e68d5616
Swati Singh <swsing@microsoft.com>
2020-12-09 01:52:57 +0530

    sixth commit

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const common_1 = require("./common");
-const models_1 = require("./models");
-const config_1 = require("./config");
+const services_1 = require("./business/services");
 class MainClass {
     static main() {
-        const customer = new models_1.Customer(1, "Northwind", "Bangalore", "info@northwind.com", "080-498349834", "SILVER", 12000, true, "Simple Remarks");
-        common_1.LogManager.info(customer.toString());
-        const settings = config_1.Configuration.getConfiguration();
-        common_1.LogManager.info(settings.getConnectionString());
+        try {
+            const service = new services_1.CustomerService();
+            const promise = service.searchCustomers("Mean");
+            promise
+                .then(records => {
+                if (records !== null) {
+                    for (const record of records) {
+                        common_1.LogManager.info(record.customerId + ", " +
+                            record.customerName);
+                    }
+                }
+            })
+                .finally(() => common_1.LogManager.info("Service Invokded Successfully!"));
+        }
+        catch (error) {
+            common_1.LogManager.error(error);
+        }
     }
 }
 MainClass.main();
--- index.ts
+++ index.ts
@@ -1,18 +1,27 @@
 import { LogManager } from "./common";
 import { Customer } from "./models";
 import { Configuration } from "./config";
+import { CustomerService } from "./business/services";
 
 class MainClass {
     static main(): void {
-        const customer = new Customer(1, "Northwind", "Bangalore",
-            "info@northwind.com", "080-498349834", "SILVER",
-            12000, true, "Simple Remarks");
+        try {
+            const service = new CustomerService();
+            const promise = service.searchCustomers("Mean");
 
-        LogManager.info(customer.toString());
-
-        const settings = Configuration.getConfiguration();
-
-        LogManager.info(settings.getConnectionString());
+            promise
+                .then(records => {
+                    if (records !== null) {
+                        for (const record of records) {
+                            LogManager.info(record.customerId + ", " +
+                                record.customerName);
+                        }
+                    }
+                })
+                .finally(() => LogManager.info("Service Invokded Successfully!"));
+        } catch (error) {
+            LogManager.error(error);
+        }
     }
 }
 
